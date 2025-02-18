3dbd421f38882125d860b1802db57011e68237d7
yusuf ginanjar <yusufginanjar2025@gmail.com>
2025-02-18 14:16:27 +0700

    implement php variable

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
+<?php
+  $pageTitle = "Pika Pika";
+  $pageDescription = "Pokemon adalah waralaba media yang dimiliki oleh Nintendo, Pokemon diciptakan oleh Satoshi Tajiri dan Ken Sugimori, dan merupakan waralaba Nintendo yang paling sukses dan menguntungkan, yang terdiri dari permainan video, kartu perdagangan, dan permainan peran (RPG) yang dimainkan oleh jutaan orang di seluruh dunia.";
+  $pageImage = "gambar-pokemon.jpeg";
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Pokemon</title>
+    <title><?= $pageTitle ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
@@ -9,8 +16,8 @@
       <div class="row">
         <div class="col-12 col-md-6 align-self-center">
           <div>
-            <h1>Pika Pika</h1>
-            <p>Pokemon adalah waralaba media yang dimiliki oleh Nintendo, Pokemon diciptakan oleh Satoshi Tajiri dan Ken Sugimori, dan merupakan waralaba Nintendo yang paling sukses dan menguntungkan, yang terdiri dari permainan video, kartu perdagangan, dan permainan peran (RPG) yang dimainkan oleh jutaan orang di seluruh dunia.</p>
+            <h1><?= $pageTitle ?></h1>
+            <p><?= $pageDescription ?></p>
           </div>
           </div>
         <div class="col-12 col-md-6">
