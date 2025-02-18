3acde59ca88d8f06bc78c1e850277ab9b860829e
yusuf ginanjar <yusufginanjar2025@gmail.com>
2025-02-18 14:05:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Pokemon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 align-self-center">
+          <div>
+            <h1>Pika Pika</h1>
+            <p>Pokemon adalah waralaba media yang dimiliki oleh Nintendo, Pokemon diciptakan oleh Satoshi Tajiri dan Ken Sugimori, dan merupakan waralaba Nintendo yang paling sukses dan menguntungkan, yang terdiri dari permainan video, kartu perdagangan, dan permainan peran (RPG) yang dimainkan oleh jutaan orang di seluruh dunia.</p>
+          </div>
+          </div>
+        <div class="col-12 col-md-6">
+          <img src="gambar-pokemon.jpeg" class="img-fluid" alt="Responsive image">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
